06c62fe1b79a4c92ee7146cce857024d9c2e8b0e
(Benja Kunkel kunkelbenjamin08@gmail.com) <kunkelbenjamin08@gmail.com>
2020-08-04 14:30:37 -0300

    Model shops con pundit y validaciones

--- app/policies/shop_policy.rb
+++ app/policies/shop_policy.rb
@@ -2,10 +2,19 @@ class ShopPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       scope.where(user_id: user.id)
-    end
+    end 
   end
-  def create?
+
+  def show?
     return true
+  end
+  
+  def new?
+    user.owner?
+  end
+
+  def create?
+    new?
   end
   
   def update?
@@ -17,5 +26,7 @@ class ShopPolicy < ApplicationPolicy
   def destroy?
     user == record.user #pregunta si el usuario que esta intentando deletear es el mismo usuario que creo el shop
   end
+
+  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'shops/new'
-  get 'shops/create'
-  get 'shops/index'
-  get 'shops/show'
-  get 'shops/edit'
-  get 'shops/update'
-  get 'shops/destroy'
+  resources :shops
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
