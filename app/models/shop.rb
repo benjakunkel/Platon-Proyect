4f7a221330ef9b66ca3e97973231c59da7e6bd1e
nicolas guelman <guelmannicolas@gmail.com>
2020-08-06 15:36:31 -0300

    agregue migraciones de longitud y config de geocoder

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -6,4 +6,6 @@ class Shop < ApplicationRecord
   validates :phone_number, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_184111) do
+ActiveRecord::Schema.define(version: 2020_08_06_183219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2020_08_04_184111) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_shops_on_user_id"
   end
 
@@ -71,6 +73,8 @@ ActiveRecord::Schema.define(version: 2020_08_04_184111) do
     t.string "phone_number"
     t.string "address"
     t.integer "role", default: 0
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
