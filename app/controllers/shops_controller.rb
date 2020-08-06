d3c670fa989a1fe2e2c32abc6c82ce400e4c5520
(Benja Kunkel kunkelbenjamin08@gmail.com) <kunkelbenjamin08@gmail.com>
2020-08-06 17:33:38 -0300

    estilos fixeados

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,7 @@
 class ShopsController < ApplicationController
- 
+  
+  skip_before_action :authenticate_user!, only: [ :index ]
+  
   def index
       @shops = policy_scope(Shop)
       @shops = Shop.all
