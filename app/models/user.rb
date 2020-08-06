a096b405035aab1673847533cd5e83cdbe5e990b
ayelen gora <ayegora@hotmail.com>
2020-08-06 19:16:55 -0300

    mergeando

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum role: [ :user, :owner, :admin ]
-<<<<<<< HEAD
   has_many :carts
-=======
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
->>>>>>> master
 end
