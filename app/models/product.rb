d25e7b6cb98cee6c6d1990c8f3ecf67cef258085
(Benja Kunkel kunkelbenjamin08@gmail.com) <kunkelbenjamin08@gmail.com>
2020-08-10 15:41:44 -0300

    agregue seeds y acomode cosas

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,6 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :unit_price, presence: true
   validates :description, presence: true
+  has_many :cart_items, dependent: :destroy
   has_one_attached :photo
 end
