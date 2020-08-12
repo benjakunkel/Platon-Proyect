0c379641314a6ba27a113ccd0e3ff7acd387addb
(Benja Kunkel kunkelbenjamin08@gmail.com) <kunkelbenjamin08@gmail.com>
2020-08-12 14:38:42 -0300

    navbar de 10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
+    # For additional fields in app/views/devise/registrations/new.html.er
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :address])  
   end
   
   def skip_pundit?
