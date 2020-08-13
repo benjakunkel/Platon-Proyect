04ea5b645ce957aad008e350812e4b58f03b1a10
nicolas guelman <guelmannicolas@gmail.com>
2020-08-13 10:09:39 -0300

    estrellas random

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_162420) do
+ActiveRecord::Schema.define(version: 2020_08_13_125536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_162420) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.integer "rating"
     t.index ["user_id"], name: "index_shops_on_user_id"
   end
 
